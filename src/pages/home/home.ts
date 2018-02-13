5f1efadf8e50a03ed2ccbd7d6b5b18e1fe6034e6
fishiflyfishi <oluwayemifisayo@gmail.com>
2018-02-13 16:56:35 +0100

    speech recognition

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,34 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  bgcolor : string = 'white';
 
+  constructor(public navCtrl: NavController, private speechRecognition: SplashScreen) {
+
+  }
+
+  ngOnInit(){
+    this.speechRecognition.hasPermission()
+    .then((hasPermission: boolean) =>{
+
+      if(!hasPermission){
+        this.speechRecognition.requestPermission()
+        .then(
+          () => console.log('Granted'),
+          () => console.log('Denied')
+        )
+      }
+    });
+  }
+
+  start(){
+    this.speechRecognition.startListening()
+    .subscribe(
+      (matches: Array<string>)=>{
+        console.log(matches);
+        this.bgcolor = matches[0]
+      }
+    )
   }
 
 }
